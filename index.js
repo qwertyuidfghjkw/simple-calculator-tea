664a26849442733b0e8aef1c0c0d84581f0ad714
dfghjkcvbnmnbvcx <162884795+qwertyuidfghjkw@users.noreply.github.com>
2024-03-17 12:07:33 +0700

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+
+function add(a, b) {
+    return a + b;
+  }
+  
+  const num1 = 5;
+  const num2 = 10;
+  const sum = add(num1, num2);
+  console.log(`Sum of ${num1} and ${num2} is ${sum}`);
+  
